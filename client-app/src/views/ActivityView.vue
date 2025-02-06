c1b29acc60b01bcaf394a1a8eb46d4d7b9110cc5
MingT1198 <xx1198@gmail.com>
2025-02-06 14:12:32 +0800

    activity開發-1

---
+++ client-app/src/views/ActivityView.vue
@@ -0,0 +1,34 @@
+<template>
+    <n-h1>This is an about page</n-h1>
+    <n-h2 v-for="activity in activitys" :key="activity.id">{{ activity }}</n-h2>
+</template>
+
+<script lang="ts">
+import type {Ref } from 'vue';
+import type { Activity } from '@/models/Activity';
+
+import axios from 'axios';
+
+import { NH1, NH2 } from 'naive-ui';
+
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({ 
+    components: {
+        NH1,
+        NH2,
+    },
+    setup() {
+        const activitys: Ref<Activity[]> = ref([]);
+
+        axios.get<Activity[]>('/api/activity').then( res => {
+            activitys.value = res.data;
+        });
+
+        return { activitys };
+    },
+});
+</script>
+
+<style>
+</style>
