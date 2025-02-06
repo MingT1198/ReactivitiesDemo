a360e468636f23aeb1d547488978c5fed0b6acd7
MingT1198 <xx1198@gmail.com>
2025-02-06 14:04:39 +0800

    加入axios套件

--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -19,8 +19,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': { // 將所有以 /api 開頭的請求代理到目標伺服器
-        target: 'http://localhost:5001', // 目標伺服器位址
+        target: 'https://localhost:5001/api', // 目標伺服器位址
         changeOrigin: true, // 必須設定，用於模擬同源請求
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, '') // 可選，重寫路徑，移除 /api 前綴
       }
     }
