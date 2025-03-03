7bd54aadc824d092e3887c91deed8264d4f1bb86
MingT1198 <xx1198@gmail.com>
2025-03-03 13:32:37 +0800

    test

--- client-app/src/models/Activity.ts
+++
@@ -1,9 +0,0 @@
-export type Activity = {
-    id: string;
-    title: string;
-    date: Date| undefined;
-    description: string | null;
-    category: string | null;
-    city: string | null;
-    venue: string | null;
-};
--- client-app/src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-   <object data="/assets/me.pdf" type="application/pdf" style="width: 100%; height: 100%;">
-   </object>
-</template>
-
-<script setup lang="ts">
-</script>
